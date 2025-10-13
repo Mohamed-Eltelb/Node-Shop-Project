31911df58a3ce617951773fdbc46a565df54df71
Flex <mohamedeltelp2017@gmail.com>
2025-10-13 08:13:49 +0300

    First commet

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path')
+const router = express.Router();
+const rootDir = require('../utils/path');
+
+const products = []
+
+router.get('/add-product',(req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+router.post('/product',(req, res, next) => {
+    products.push(req.body.title)
+    res.redirect('/');
+});
+
+module.exports = router;
